3bdcfa9e8592481521cd677189a953c8de2b577c
andyschauer <aschauer@uw.edu>
2024-04-16 15:52:01 -0700

    Initial commit

---
+++ shrekCN.py
@@ -0,0 +1,437 @@
+#!/usr/bin/env python3
+"""
+This script opens raw carbon and nitrogen stable isotope elemental analysis data
+files from shrek, a ThermoFinnigan 253 with Eurovector Elemental Analyzer, and
+writes salient data to shrekCN_analysis_log.csv
+
+Version 0.1 mod date 2021-02-03 => created
+Version 0.2 mod date 2021-02-25 => working version
+Version 0.3 mod date 2021-03-01 => added Nonly and Conly, moves raw file to either archive or unread directories,
+        tested it with 2008 to 2018 data
+Version 0.4 mod date 2021-03-03 => changed Height to Ampl28 and Ampl44
+Version 0.5 mod date 2021-10-08 => updating nomenclature
+Version 0.6 mod date 2022-02-17 => updating, linting, more nomenclature
+Version 0.7 mod date 2022-03-23 => export data and headers comes from shrekCN_standards.py; script tries to detect
+        computer you are working on and adjust directories accordingly;
+Version 0.8 mod date 2022-12-31 => updated, linted, tested. I think this now works after a complete reprocess of all shrek CN data.
+Version 1.0 mod date 2024-04-08 => changed lab to isolab_lib; changed flag to trust, some refactoring for readability,
+        function to handle sample notestrying to get this finished up to a version 1 level and upload to github
+"""
+
+__author__ = "Andy Schauer"
+__email__ = "aschauer@uw.edu"
+__last_modified__ = "2024-04-12"
+__version__ = "1.0"
+__copyright__ = "Copyright 2024, Andy Schauer"
+__license__ = "Apache 2.0"
+__acknowledgements__ = "Shrek"
+
+
+
+
+# -------------------- imports --------------------
+import csv
+import isolab_lib
+import os
+import re
+from shrekCN_lib import *
+from shrekCN_standards import *
+import sys
+import time
+
+
+
+# -------------------- functions --------------------
+def append_meta_data():
+    for i in meta_data:
+        meta_data[i].append(data[i][index]) if i in data else meta_data[i].append(None)
+
+
+def append_supp_data():
+    supp_data['file'].append(file)
+    supp_data['trust'].append(trust)
+    supp_data['notes'].append(note)
+    supp_data['pyversions'].append(version)
+    supp_data['empty'].append('')
+
+    if 'Information' in data:
+        if data['Information'][index] is None:
+            supp_data['peak_center'].append(None)
+        elif re.match('Peak Center found at', data['Information'][index]) is None:
+            supp_data['peak_center'].append(None)
+        else:
+            m = re.findall(r'\d+', data['Information'][index])
+            supp_data['peak_center'].append(m[0])
+    else:
+        supp_data['peak_center'].append(None)
+
+
+def append_N_wg_data():  # put data for nitrogen refence peak into list
+    for i in N_wg_data:
+        N_wg_data[i].append(data[i][index + peak_number_offset]) if i in data else N_wg_data[i].append(None)
+
+
+def append_N_sam_data():  # put data for nitrogen sample peak into list
+    for i in N_sam_data:
+        N_sam_data[i].append(data[i][index + peak_number_offset]) if i in data else N_sam_data[i].append(None)
+    if int(data['Ampl28'][index + peak_number_offset]) > 49950 or int(data['Ampl29'][index + peak_number_offset]) > 49950:
+        global trust
+        trust = 0
+        sample_note('N2 cup saturated')
+
+
+def append_C_sam_data():  # put data for carbon sample peak into list
+    for i in C_sam_data:
+        C_sam_data[i].append(data[i][index + peak_number_offset]) if i in data else C_sam_data[i].append(None)
+    if int(data['Ampl44'][index + peak_number_offset]) > 49950 or int(data['Ampl45'][index + peak_number_offset]) > 49950 or int(data['Ampl46'][index + peak_number_offset]) > 49950:
+        global trust
+        trust = 0
+        sample_note('CO2 cup saturated')
+
+
+def append_C_wg_data():  # put data for carbon refernce peak into list
+    for i in C_wg_data:
+        C_wg_data[i].append(data[i][index + peak_number_offset]) if i in data else C_wg_data[i].append(None)
+
+
+def N_wg_none():  # sets all nitrogen reference peak data to None
+    for i in N_wg_data:
+        N_wg_data[i].append(None)
+
+
+def N_sam_none():  # sets all nitrogen sample peak data to None
+    for i in N_sam_data:
+        N_sam_data[i].append(None)
+
+
+def C_sam_none():  # sets all carbon sample peak data to None
+    for i in C_sam_data:
+        C_sam_data[i].append(None)
+
+
+def C_wg_none():  # sets all carbon reference peak data to None
+    for i in C_wg_data:
+        C_wg_data[i].append(None)
+
+
+def sample_note(currnote):
+    if trust == 0:
+        print(f"\033[91m Analysis {str(data['Analysis'][index])} in file {file} - {currnote}\033[0m")
+    else:
+        print(f" Analysis {str(data['Analysis'][index])} in file {file} - {currnote}")
+    note.append(currnote)
+
+
+
+# -------------------- setup --------------------
+start_time = time.time()
+version = os.path.basename(__file__) + ' - ' + time.ctime(os.path.getctime(__file__))
+project_directory = isolab_lib.get_path("shrekCN", "project")
+new_data_directory = 'rawdata_new'
+archive_data_directory = 'rawdata_archive'
+junk_data_directory = 'rawdata_junk'
+log_file_name = 'shrekCN_analysis_log.csv'
+
+if os.path.isdir(project_directory) is False:
+    print('directory does not exist...exiting....')
+    sys.exit()
+
+
+
+# -------------------- get list of files --------------------
+filelist = isolab_lib.make_file_list(os.path.join(project_directory, new_data_directory), 'csv')
+if not filelist:
+    print('No files in raw data directory.')
+else:
+    filelist = sorted(filelist)
+
+
+
+# -------------------- Main loop --------------------
+for file in filelist:
+    for i in meta_headers:
+        meta_data[i] = []
+
+    for i in N_headers:
+        N_wg_data[i] = []
+        N_sam_data[i] = []
+
+    for i in C_headers:
+        C_wg_data[i] = []
+        C_sam_data[i] = []
+
+    for i in supp_headers:
+        supp_data[i] = []
+
+    headers, data = isolab_lib.read_file(os.path.join(project_directory, new_data_directory, file), ',')  # read file and return headers (headers) and data (data)
+
+    # test for file problems
+    if 'Analysis' not in headers:
+        print('problem with file ' + file)
+        os.rename(os.path.join(project_directory, new_data_directory, file), os.path.join(project_directory, junk_data_directory, file))  # done with datafile, put it in the unread directory
+        print('file ' + file + ' was moved to the junk folder')
+        continue
+
+    for n in data['Analysis']:
+        try:
+            data['Analysis'] = [int(index) for index in data['Analysis']]
+        except ValueError:
+            print(f'File {file} contains strings in the Analysis column')
+            os.rename(os.path.join(project_directory, new_data_directory, file), os.path.join(project_directory, junk_data_directory, file))  # done with datafile, put it in the unread directory
+            print(f'File {file} was moved to the junk folder.')
+            continue
+
+    # sample_index_first_row = first row of each sample
+    analysis_difference = [j - index for index, j in zip(data['Analysis'][: -1], data['Analysis'][1:])]
+    sample_index_first_row = [index for index, e in enumerate(analysis_difference) if e != 0]
+    sample_index_first_row = [index + 1 for index in sample_index_first_row]
+    sample_index_first_row.append(0)
+    sample_index_first_row = sorted(sample_index_first_row)
+
+    # rows_per_sample
+    rows_per_sample = [j - index for index, j in zip(sample_index_first_row[:-1], sample_index_first_row[1:])]
+    rows_per_sample.append(len(data['Analysis']) - sample_index_first_row[len(sample_index_first_row) - 1])  # adds last sample number of rows
+
+    # sample_index_last_row = last row of each sample
+    sample_index_last_row = [j + index for index, j in zip(sample_index_first_row, rows_per_sample)]
+
+    # run type - N, C, CN
+    if 'Gasconfiguration' in data:
+        gas_configuration = set(data['Gasconfiguration'])
+    else:
+        gas_configuration = 'undefined'
+
+    if any(index in ['N2', 'CO2'] for index in gas_configuration):
+        if all(index in gas_configuration for index in ['N2', 'CO2']):
+            run_type = 'CN'
+        elif 'N2' not in gas_configuration:
+            run_type = 'C'
+        elif 'CO2' not in gas_configuration:
+            run_type = 'N'
+    else:
+        run_type = 'unk'
+
+    if any([run_type == 'CN', run_type == 'N', run_type == 'C']):  # only run this code if the run_type is CN, N, or C
+
+        for index, rows in zip(sample_index_first_row, rows_per_sample):
+            append_meta_data()
+            note = []
+            trust = 1
+
+            # Identify peaks based on run type and number of peaks per analysis
+            if run_type == 'CN':
+
+                if rows == 1:  # 1 peak in CN mode means problems, so don't export any data and let user know
+                    trust = 0
+                    sample_note('Only one peak detected; ')
+                    peak_number_offset = 0
+                    N_wg_none()
+                    N_sam_none()
+                    C_sam_none()
+                    C_wg_none()
+
+                elif rows == 2:  # 2 peaks in a CN run could mean Nref and Cref, or a problem happened and there are no carbon peaks, index can't think of a scenario in a CN run where you would have no nitrogen peaks but would have carbon peaks
+                    # PeakNr1 is Nref
+                    peak_number_offset = 0
+                    append_N_wg_data()
+
+                    # PeakNr2 could be Nsam or Cref or some other problem
+                    peak_number_offset = 1
+                    if data['Gasconfiguration'][index + peak_number_offset] == 'N2':  # Nsam is present but Csam is missing
+                        sample_note('No carbon peaks')
+                        append_N_sam_data()
+                        C_sam_none()
+                        C_wg_none()
+                    elif data['Gasconfiguration'][index + peak_number_offset] == 'CO2':  # Nsam and Csam are missing but Cref is present
+                        sample_note('No nitrogen or carbon sample peaks')
+                        N_sam_none()
+                        C_sam_none()
+                        append_C_wg_data()
+
+                elif rows == 3:  # second most common scenario where peaks are either Nref, Nsam, Cref or Nref, Csam, Cref
+                    # PeakNr1 is Nref
+                    peak_number_offset = 0
+                    append_N_wg_data()
+
+                    # PeakNr2
+                    peak_number_offset = 1
+                    if data['Gasconfiguration'][index + peak_number_offset] == 'N2':  # Nsam is present but Csam is missing
+                        append_N_sam_data()
+                        C_sam_none()
+                    elif data['Gasconfiguration'][index + peak_number_offset] == 'CO2':  # Nsam is missing but Csam is present
+                        N_sam_none()
+                        append_C_sam_data()
+
+                    # PeakNr3 is Cref
+                    peak_number_offset = 2
+                    append_C_wg_data()
+
+                elif rows == 4:  # most common scenario where peaks are Nref, Nsam, Csam, Cref
+                    # PeakNr1 is Nref
+                    peak_number_offset = 0
+                    append_N_wg_data()
+
+                    # PeakNr2 is Nsam
+                    peak_number_offset = 1
+                    append_N_sam_data()
+
+                    # PeakNr3 is Csam
+                    peak_number_offset = 2
+                    append_C_sam_data()
+
+                    # PeakNr4 is Cref
+                    peak_number_offset = 3
+                    append_C_wg_data()
+
+                elif rows == 5:
+                    # determine which gas config has extra peak then assign peaks and exclude extra but make note of it
+                    if data['Gasconfiguration'][index:index + rows].count('N2') == 2:  # CO2 has extra peak
+                        sample_note('extra CO2 peak detected')
+                        # PeakNr1 is Nref
+                        peak_number_offset = 0
+                        append_N_wg_data()
+
+                        # PeakNr2 is Nsam
+                        peak_number_offset = 1
+                        append_N_sam_data()
+
+                        # PeakNr 3 or 4 is Csam
+                        test = data['AreaAll'][index + peak_number_offset + 1] > data['AreaAll'][index + peak_number_offset + 2] if type(data['AreaAll'][index + peak_number_offset + 1]) == int and type(data['AreaAll'][index + peak_number_offset + 2]) == int else None
+                        if test is True or test is False:  # if peak 3 is larger than 4, peak 3 is sample
+                            if test is True:
+                                peak_number_offset = 2
+                                sample_note('using larger first of two sample peaks')
+                            elif test is False:
+                                peak_number_offset = 3
+                                sample_note('using larger second of two sample peaks')
+                            append_C_sam_data()
+
+                        else:
+                            trust = 0
+                            sample_note('problem with AreaAll data')
+                            C_sam_none()
+
+                        # PeakNr5 is Cref
+                        peak_number_offset = 4
+                        append_C_wg_data()
+
+                    elif data['Gasconfiguration'][index:index + rows].count('CO2') == 2:  # N2 has extra peak
+                        N_wg_none()
+                        N_sam_none()
+                        # PeakNr3 is Csam
+                        peak_number_offset = 3
+                        append_C_sam_data()
+
+                        # PeakNr4 is Cref
+                        peak_number_offset = 4
+                        append_C_wg_data()
+                        sample_note('5 peaks in sample and N2 has extra')
+                    else:
+                        N_wg_none()
+                        N_sam_none()
+                        C_sam_none()
+                        C_wg_none()
+                        trust = 0
+                        sample_note('5 peaks in sample but something unexpected happened')
+
+                elif rows == 6:
+                    N_wg_none()
+                    N_sam_none()
+                    C_sam_none()
+                    C_wg_none()
+                    trust = 0
+                    sample_note('6 peaks in sample')
+
+                else:
+                    N_wg_none()
+                    N_sam_none()
+                    C_sam_none()
+                    C_wg_none()
+                    trust = 0
+                    sample_note('too many peaks causes anxiety')
+
+            elif run_type == 'N':
+                C_sam_none()
+                C_wg_none()
+                if rows == 4:
+                    # PeakNr2 and 5 are Nref but index need to build in averaging these two peaks, for now it is PeakNr2
+                    peak_number_offset = 1
+                    append_N_wg_data()
+                    N_sam_none()
+
+                elif rows == 5:
+                    # PeakNr2 and 5 are Nref but index need to build in averaging these two peaks, for now it is PeakNr2
+                    peak_number_offset = 1
+                    append_N_wg_data()
+
+                    # PeakNr4 is Nsam
+                    peak_number_offset = 4
+                    append_N_sam_data()
+                    sample_note('No nitrogen sample peaks')
+
+                else:
+                    N_wg_none()
+                    N_sam_none()
+                    trust = 0
+                    sample_note('Number of peaks suggests an error')
+
+            elif run_type == 'C':
+                N_wg_none()
+                N_sam_none()
+                if rows == 4:
+                    # PeakNr2 and 5 are Nref but index need to build in averaging these two peaks, for now it is PeakNr2
+                    peak_number_offset = 1
+                    append_C_wg_data()
+                    C_sam_none()
+
+                elif rows == 5:
+                    # PeakNr2 and 5 are Nref but index need to build in averaging these two peaks, for now it is PeakNr2
+                    peak_number_offset = 1
+                    append_C_wg_data()
+
+                    # PeakNr4 is Csam
+                    peak_number_offset = 4
+                    append_C_sam_data()
+                    sample_note('No carbon sample peaks')
+
+                else:
+                    C_wg_none()
+                    C_sam_none()
+                    trust = 0
+                    sample_note('Number of peaks suggests and error')
+
+            else:  # run type is not known or something about identifying it went wrong
+                N_wg_none()
+                N_sam_none()
+                C_sam_none()
+                C_wg_none()
+                trust = 0
+                sample_note('Run type unknown')
+
+            note = ", ".join(note)
+            append_supp_data()
+
+        if os.path.isfile(os.path.join(project_directory, log_file_name)) is False:
+            with open(os.path.join(project_directory, log_file_name), 'w', newline='') as csvfile:  # if the log file has not been created, create it with column headers and data
+                datawriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+                datawriter.writerow(shrekCN_analysis_log_headers)
+                for ii in range(len(meta_data['Analysis'])):
+                    datawriter.writerow(eval(data_to_write))
+
+        else:
+            with open(os.path.join(project_directory, log_file_name), 'a', newline='') as csvfile:
+                datawriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+                for ii in range(len(meta_data['Analysis'])):
+                    datawriter.writerow(eval(data_to_write))
+
+        # in addition to the above project specific log file to write sample data to, write to this one that has all data ever
+        # with open(os.path.join(project_directory, 'shrekCN_analysis_log.csv'), 'a', newline='') as csvfile:
+        #     datawriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        #     for ii in range(len(meta_data['Analysis'])):
+        #         datawriter.writerow(eval(data_to_write))
+
+        os.rename(os.path.join(project_directory, new_data_directory, file), os.path.join(project_directory, archive_data_directory, file))  # done with datafile, put it in the archive directory
+    else:
+        os.rename(os.path.join(project_directory, new_data_directory, file), os.path.join(project_directory, junk_data_directory, file))  # done with datafile, put it in the unread directory
+
+print("--- %s seconds ---" % (time.time() - start_time))
---
+++ shrekCN_lib.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+Random items needed by both shrekCN.py and shrekCNcalibrate.py
+"""
+
+__author__ = "Andy Schauer"
+__email__ = "aschauer@uw.edu"
+__last_modified__ = "2024-04-16"
+__version__ = "1.0"
+__copyright__ = "Copyright 2024, Andy Schauer"
+__license__ = "Apache 2.0"
+
+
+refmat_list = ['NIST1547', 'GA1', 'GA2', 'SA', 'MAL', 'DSM']
+knowns_list = refmat_list[:] + ['zero', 'qtycal', 'blank', 'emptytin']
+
+meta_headers = ['Amount', 'Analysis', 'Comment', 'Date', 'Identifier1', 'Identifier2', 'Information', 'Line', 'Method', 'Row', 'Time']
+
+N_headers = ['AT15N14N', 'Ampl28', 'Ampl29', 'AreaAll', 'Area28', 'Area29', 'BGD28', 'BGD29', 'Gasconfiguration', 'R15N14N', 'R29N228N2', 'd15N14N',
+             'd29N228N2', 'PeakNr', 'rArea28', 'rArea29', 'rR29N228N2', 'rd29N228N2', 'Start', 'Width']
+
+C_headers = ['AT13C12C', 'Ampl44', 'Ampl45', 'Ampl46', 'AreaAll', 'Area44', 'Area45', 'Area46', 'BGD44', 'BGD45', 'BGD46', 'Gasconfiguration',
+             'PeakNr', 'R13C12C', 'R45CO244CO2', 'R46CO244CO2', 'd13C12C', 'd45CO244CO2', 'd46CO244CO2',
+             'rArea45', 'rR45CO244CO2', 'rR46CO244CO2', 'rd45CO244CO2', 'rd46CO244CO2', 'Start', 'Width']
+
+supp_headers = ['file', 'trust', 'notes', 'peak_center', 'pyversions', 'empty']
+
+numlist = ['Amount', 'Analysis', 'Line','Row', 'N_wg_AT15N14N', 'N_wg_Ampl28', 'N_wg_Ampl29', 'N_wg_AreaAll', 'N_wg_Area28', 'N_wg_Area29', 'N_wg_BGD28', 'N_wg_BGD29',
+           'N_wg_R15N14N', 'N_wg_R29N228N2', 'N_wg_d15N14N', 'N_wg_d29N228N2', 'N_wg_PeakNr', 'N_wg_rArea28', 'N_wg_rArea29', 'N_wg_rR29N228N2', 'N_wg_rd29N228N2', 'N_wg_Start',
+           'N_wg_Width', 'N_sam_AT15N14N', 'N_sam_Ampl28', 'N_sam_Ampl29', 'N_sam_AreaAll', 'N_sam_Area28', 'N_sam_Area29', 'N_sam_BGD28', 'N_sam_BGD29', 'N_sam_R15N14N',
+           'N_sam_R29N228N2', 'N_sam_d15N14N', 'N_sam_d29N228N2', 'N_sam_PeakNr', 'N_sam_rArea28', 'N_sam_rArea29', 'N_sam_rR29N228N2', 'N_sam_rd29N228N2', 'N_sam_Start',
+           'N_sam_Width', 'C_sam_AT13C12C', 'C_sam_Ampl44', 'C_sam_Ampl45', 'C_sam_Ampl46', 'C_sam_AreaAll', 'C_sam_Area44', 'C_sam_Area45', 'C_sam_Area46', 'C_sam_BGD44',
+           'C_sam_BGD45', 'C_sam_BGD46', 'C_sam_PeakNr', 'C_sam_R13C12C', 'C_sam_R45CO244CO2', 'C_sam_R46CO244CO2', 'C_sam_d13C12C', 'C_sam_d45CO244CO2',
+           'C_sam_d46CO244CO2', 'C_sam_rArea45', 'C_sam_rR45CO244CO2', 'C_sam_rR46CO244CO2', 'C_sam_rd45CO244CO2', 'C_sam_rd46CO244CO2', 'C_sam_Start', 'C_sam_Width',
+           'C_wg_AT13C12C', 'C_wg_Ampl44', 'C_wg_Ampl45', 'C_wg_Ampl46', 'C_wg_AreaAll', 'C_wg_Area44', 'C_wg_Area45', 'C_wg_Area46', 'C_wg_BGD44', 'C_wg_BGD45', 'C_wg_BGD46',
+           'C_wg_PeakNr', 'C_wg_R13C12C', 'C_wg_R45CO244CO2', 'C_wg_R46CO244CO2', 'C_wg_d13C12C', 'C_wg_d45CO244CO2', 'C_wg_d46CO244CO2', 'C_wg_rArea45', 'C_wg_rR45CO244CO2',
+           'C_wg_rR46CO244CO2', 'C_wg_rd45CO244CO2', 'C_wg_rd46CO244CO2', 'C_wg_Start', 'C_wg_Width', 'trust', 'peak_center']
+
+qtycal = {'names': ['qtycal_GA1', 'qtycal_ga1', 'qtycal.GA1'],
+          'material': 'GA1',
+          'index': [],
+          'fractionN': 0.0952,
+          'fractionC': 0.4082,
+          'notes': 'material weighed across a range to calibrate peak area to quantity'}
+
+blank = {'names': ['blank'],
+         'material': None,
+         'notes': 'no material dropped into EA'}
+
+emptytin = {'names': ['empty_tin', 'Empty Tin'],
+            'material': 'tin cups',
+            'percentN': None,
+            'percentC': None}
+
+zero = {'names': ['zero'],
+        'material': 'reference gas peaks treated as unknowns'}
+
+
+
+meta_data = {}
+N_wg_data = {}
+N_sam_data = {}
+C_sam_data = {}
+C_wg_data = {}
+supp_data = {}
+
+# prepare export file column headers
+shrekCN_analysis_log_headers = []
+shrekCN_analysis_log_headers.extend(meta_headers)
+shrekCN_analysis_log_headers.extend([f"N_wg_{i}" for i in N_headers])
+shrekCN_analysis_log_headers.extend([f"N_sam_{i}" for i in N_headers])
+shrekCN_analysis_log_headers.extend([f"C_sam_{i}" for i in C_headers])
+shrekCN_analysis_log_headers.extend([f"C_wg_{i}" for i in C_headers])
+shrekCN_analysis_log_headers.extend(supp_headers)
+
+data_to_write = []
+data_to_write.extend([f"meta_data['{i}'][ii]" for i in meta_headers])
+data_to_write.extend([f"N_wg_data['{i}'][ii]" for i in N_headers])
+data_to_write.extend([f"N_sam_data['{i}'][ii]" for i in N_headers])
+data_to_write.extend([f"C_sam_data['{i}'][ii]" for i in C_headers])
+data_to_write.extend([f"C_wg_data['{i}'][ii]" for i in C_headers])
+data_to_write.extend([f"supp_data['{i}'][ii]" for i in supp_headers])
+data_to_write = str(data_to_write).replace("\"", "")
